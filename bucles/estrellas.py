dd37f92326ed40f2810c1e0c030c721aded31686
Max23Esau <esa-max23@live.com>
2020-11-25 21:24:22 -0600

    add moño in estrellas.py

--- bucles/estrellas.py
+++ bucles/estrellas.py
@@ -2,31 +2,67 @@
 # contador_1 = 1
 # contador_2 = 10
 
-# while contador_1 <= 10:
+# while contador_1 < 10:
 #     print('* ' * contador_1)
 #     contador_1 = contador_1 + 1
 
-# while contador_2 >= 1:
+# while contador_2 > 1:
 #     print('* ' * contador_2)
-#     contador_2 = contador_2 - 1 
-#------------------------------------------------------------------------    
+#     contador_2 = contador_2 - 1
+#------------------------------------------------------------------------
 
-#------------------------------------------------------------------------    
+#------------------------------------------------------------------------
+# #Piramide a la derecha
+# contador_1 = 1
+# contador_2 = 10
+# espacios = 20
+# while contador_1 < 10:
+#     print(' ' * espacios + '* ' * contador_1)
+#     contador_1 = contador_1 + 1
+#     espacios = espacios - 2
+
+# espacios = 2
+# while contador_2 >= 1:
+#     print(' ' * espacios + '* ' * contador_2)
+#     contador_2 = contador_2 - 1
+#     espacios = espacios + 2
+
+#------------------------------------------------------------------------
+
+#------------------------------------------------------------------------
+#Moño
+contador_1 = 1
+contador_2 = 10
+espacios = 18
+
+while contador_1 <= 10:
+    print('* ' * contador_1 + ' ' * espacios + ' ' * espacios + '* ' * contador_1)
+    contador_1 = contador_1 + 1
+    espacios = espacios - 2
+espacios = 0
+while contador_2 >= 1:
+    print('* ' * contador_2 + ' ' * espacios + ' ' * espacios + '* ' * contador_2)
+    contador_2 = contador_2 - 1
+    espacios = espacios + 2
+
+#------------------------------------------------------------------------
+
+#------------------------------------------------------------------------
 #Piramide de asteriscos
 # contador_1 = 1
 # espacios = 10
 
-# while contador_1 <= 10:
+# while contador_1 < 10:
 #     print(' ' * espacios + '* ' * contador_1)
 #     contador_1 = contador_1 + 1
 #     espacios = espacios - 1
 #------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------    
+#------------------------------------------------------------------------
 #Romboide de asteriscos
 # contador_1 = 1
 # espacios = 10
-# while contador_1 <= 10:
+# while contador_1 < 10:
 #     print(' ' * espacios + '* ' * contador_1)
 #     contador_1 = contador_1 + 1
 #     espacios = espacios - 1
@@ -39,7 +75,7 @@
 #     espacios = espacios + 1
 #------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------    
+#------------------------------------------------------------------------
 #Contorno de un romboide
 # contador_1 = 10
 # espacios = 0
